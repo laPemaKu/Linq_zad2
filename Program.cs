ad47773798927a479d2aa74c13b946efa4013d74
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-18 11:04:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+	public static void Main()
+	{
+        Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+        var linqStudents = from student in studentArray
+                           orderby student.Age descending
+                           select student;
+
+        foreach(var student in linqStudents)
+        {
+            Console.WriteLine(student.Age);
+        }
+
+        Console.ReadKey();
+	}
+}
+
+
+public class Student
+{
+
+	public int StudentID { get; set; }
+	public string StudentName { get; set; }
+    public int Age { get; set; }
+}
